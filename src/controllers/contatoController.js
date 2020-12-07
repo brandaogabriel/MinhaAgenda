36851ebccb03def543663068d2824854b7dad7e4
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2020-12-07 11:12:33 -0300

    add editar, listar e remover um contato

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -4,7 +4,7 @@ exports.index = (req, res) => {
   res.render('contato', {
     contato: {}
   });
-}
+};
 
 exports.register = async (req, res) => {
 
@@ -21,13 +21,14 @@ exports.register = async (req, res) => {
   
     req.flash('success', 'Contato registrado com sucesso.');
     req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));  
+    return;
 
   }catch(e){
     console.log(e);
     res.render('404');
   }
 
-}
+};
 
 exports.editIndex = async (req, res) => {
   if(!req.params.id) return res.render('404');
@@ -38,4 +39,41 @@ exports.editIndex = async (req, res) => {
   
   res.render('contato', { contato });
 
-}
+};
+
+exports.edit = async (req, res) => {
+
+  try{
+
+    if(!req.params.id) return res.render('404');
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
+  
+    if(contato.errors.length > 0){
+      req.flash('errors', contato.errors);
+      req.session.save(() => res.redirect('back'));
+      return; 
+    }
+  
+    req.flash('success', 'Contato editado com sucesso.');
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));  
+    return;
+
+  }catch(e){
+    console.log(e);
+    res.render('404');
+  }
+
+};
+
+exports.delete = async (req, res) => {
+  if(!req.params.id) return res.render('404');
+  
+  const contato = await Contato.delete(req.params.id);
+
+  if(!contato) return res.render('404');
+
+  req.flash('success', 'Contato apagado com sucesso.');
+  req.session.save(() => res.redirect('back'));  
+  return;
+};
--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -17,12 +17,6 @@ function Contato(body){
   this.contato = null;
 }
 
-Contato.buscaPorId = async function(id){
-  if(typeof id !== 'string') return;
-  const user = await ContatoModel.findById(id);
-  return user;
-}
-
 Contato.prototype.register = async function(){
   this.valida();
 
@@ -52,6 +46,33 @@ Contato.prototype.cleanUp = function() {
     email: this.body.email,
     telefone: this.body.telefone
   }
+}
+
+Contato.prototype.edit = async function(id){
+  if(typeof id !== 'string') return;
+  this.valida();
+  if(this.errors.length > 0) return;
+
+  this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true });
+}
+
+// Métodos estáticos
+Contato.buscaPorId = async function(id){
+  if(typeof id !== 'string') return;
+  const contato = await ContatoModel.findById(id);
+  return contato;
+}
+
+Contato.buscaContatos = async function(){
+  const contatos = await ContatoModel.find()
+    .sort({ criadoEm: -1 });
+  return contatos;
+}
+
+Contato.delete = async function(id){
+  if(typeof id !== 'string') return;
+  const contato = await ContatoModel.findByIdAndDelete({ _id: id });
+  return contato;
 }
 
 
